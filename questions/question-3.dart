108b6cd51c2f625a58969f2c086393e7675226f3
Larissa Corrẽa <larimscorrea@gmail.com>
2023-04-18 23:32:57 -0300

    dart

---
+++ questions/question-3.dart
@@ -0,0 +1,13 @@
+// Escreva um programa em Dart que leia um número inteiro e informe se ele é par ou ímpar.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Escreva um número? ');
+  int number = int.parse(stdin.readLineSync());
+
+  int operation =
+
+  if()  
+  print('Você tem $idade anos.');
+}
