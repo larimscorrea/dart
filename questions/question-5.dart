fdb4da2261ecfca3c071e821338c30a292950191
Larissa Corrẽa <larimscorrea@gmail.com>
2023-04-22 22:57:13 -0300

    question 5

---
+++ questions/question-5.dart
@@ -0,0 +1,15 @@
+// Escreva um programa em Dart que leia dois números inteiros e exiba todos os números ímpares entre eles.
+import 'dart:io';
+
+void main() {
+  stdout.write('Escreva um número? ');
+  int number1 = int.parse(stdin.readLineSync());
+  int number2 = int.parse(stdin.readLineSync());
+
+  function operation() {
+    (if number1 != 0 & number2 != 0) {
+    }
+  }
+  
+  print('O resultado do fatorial é: $operation');
+}
