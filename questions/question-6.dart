909c3ad0c2c7ad9bed63e3fc8df5f8071dc0854c
Larissa Corrẽa <larimscorrea@gmail.com>
2023-06-18 20:11:03 -0300

    more a question

--- questions/question-6.dart
+++ questions/question-6.dart
@@ -1,1 +1,16 @@
 // Escreva um programa em Dart que leia dois números inteiros e informe se o primeiro é múltiplo do segundo.
+import 'dart:io';
+
+void main() {
+  print('Digite o primeiro número: ');
+  int primeiroNumero = int.parse(stdin.readLineSync()!);
+
+  print('Digite o segundo número: ');
+  int segundoNumero = int.parse(stdin.readLineSync()!);
+
+  if (primeiroNumero % segundoNumero == 0) {
+    print('$primeiroNumero é múltiplo de $segundoNumero');
+  } else {
+    print('$primeiroNumero não é múltiplo de $segundoNumero');
+  }
+}
